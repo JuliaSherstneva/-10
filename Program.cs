65ef456ed64bddcd4fa0a1d18c2aabfd75000db1
исп-22 <-22@LAB30-04>
2024-12-21 11:50:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// Записать элементы матрицы построчно в одномерный массив и найти его минимальный элемент.
+
+       int[,] matrix = { 
+            { 1, 2, 3 }, 
+            { 4, 5, 6 }, 
+            { 7, 8, 9 } 
+                        }; 
+        
+       int Stroka = matrix.GetLength(0); 
+       int Stolb = matrix.GetLength(1); 
+       
+       int[] array = new int[Stroka * Stolb]; 
+        
+       int index = 0; 
+            for (int i = 0; i < Stroka; i++) 
+        { 
+            for (int j = 0; j < Stolb; j++) 
+            { 
+                array[index++] = matrix[i, j]; 
+            } 
+        } 
+       int min = array[0]; 
+            for (int i = 1; i < array.Length; i++) 
+        { 
+            if (array[i] < min) 
+            { 
+                min = array[i]; 
+            } 
+        } 
+          Console.WriteLine("Минимальный элемент: " + min); 
